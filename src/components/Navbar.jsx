d6310d11050d9b110128baf37ab92c34eb91a94b
Tahahamdii <hamdi.taha@esprit.tn>
2023-04-27 22:49:06 +0200

    first commit

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import { Button, Menu, Typography,Avatar} from 'antd';
+import { Link } from 'react-router-dom';
+import { HomeOutlined, MoneyCollectOutlined, BulbOutlined, FundOutlined,MenuOutlined } from '@ant-design/icons';
+import icon from '../images/logo.png';
+const Navbar = () => {
+  return (
+    <div className='nav-container'>
+        <div className='logo-container'>
+            <Avatar src={icon} size="large" />
+            <Typography.Title level={2} className='logo'>
+                <Link to="/">Finovo</Link>
+            </Typography.Title>
+        </div>
+
+    </div>
+  )
+}
+
+export default Navbar
